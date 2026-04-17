7300f66d81c10ea65b81d2802f6d563e38b94a2b
mozaragencia-dot <mozaragencia@gmail.com>
2026-04-17 10:56:21 -0400

    Update Gendarmeria roster template to new signed day-ahead layout

--- storage/app.js
+++ storage/app.js
@@ -2357,6 +2357,7 @@ function buildGendarmeriaTemplateData(visits) {
     fechaHoy: String(safeVisits[0]?.date || getTomorrowDateString()),
     totalVisitas: String(Array.isArray(visits) ? visits.length : 0),
     folioDocumento: `TAC-${folioBase}`,
+    abogadaFirma: '',
     visits: safeVisits.map((booking, index) => ({
       numero: index + 1,
       hora: String(booking?.time || '--:--'),
